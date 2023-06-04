22c218582f52d89a40965cab893625683c73522d
SandWithCheese <naufalahmad022@gmail.com>
2023-06-04 13:21:46 +0700

    Fixing LinkedIn Link

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -15,7 +15,7 @@ export default function Contacts() {
 
             <h3 className="text-center mt-12 text-2xl">Contacts</h3>
             <div className="flex justify-around mt-8 pb-12 w-64 mx-auto">
-                <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/ahmad-naufal-ramadan-aa1209238/">
+                <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/ahmad-naufal-ramadan/">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="currentColor" viewBox="0 0 24 24">
                         <path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
                     </svg>
