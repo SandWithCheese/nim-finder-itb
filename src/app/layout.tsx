952f6871fe49cdd0828756a28f6b695af09f795b
SandWithCheese <naufalahmad022@gmail.com>
2023-06-06 16:00:03 +0700

    Adding google verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import './globals.css'
 export const metadata = {
   title: 'NIM Finder',
   description: 'NIM Finder Untuk Mahasiswa ITB',
+  verification: {
+    google: "fv_CNbFwrtMZ1V0Z2RV4p3t48ULjscLJ97A_P08DT8E"
+  },
 }
 
 export default function RootLayout({
